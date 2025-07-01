42db6794da1f56c27b9a6f29d3b5f3ab57d564f5
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-01 19:34:14 +0300

    попытка удалить educational....

--- src/course-home/outline-tab/SequenceLink.jsx
+++ src/course-home/outline-tab/SequenceLink.jsx
@@ -116,21 +116,7 @@ const SequenceLink = ({
             <EffortEstimate className="ml-3 align-middle" block={sequence} />
           </div>
         </div>
-        {hideFromTOC && (
-          <div className="row w-100 my-2 mx-4 pl-3">
-            <span className="small d-flex">
-              <Icon className="mr-2" src={Block} data-testid="hide-from-toc-sequence-link-icon" />
-              <span data-testid="hide-from-toc-sequence-link-text">
-                {intl.formatMessage(messages.hiddenSequenceLink)}
-              </span>
-            </span>
-          </div>
-        )}
-        {/* <div className="row w-100 m-0 ml-3 pl-3">
-          <small className="text-body pl-2">
-            {due ? dueDateMessage : noDueDateMessage}
-          </small>
-        </div> */}
+
       </div>
     </li>
   );
