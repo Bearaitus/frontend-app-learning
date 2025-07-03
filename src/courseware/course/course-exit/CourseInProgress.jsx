0d5d4a6844f2d3365aba780a9035f1f231a6769b
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-03 14:39:53 +0300

    только название страницы

--- src/courseware/course/course-exit/CourseInProgress.jsx
+++ src/courseware/course/course-exit/CourseInProgress.jsx
@@ -32,7 +32,7 @@ const CourseInProgress = ({ intl }) => {
   return (
     <>
       <Helmet>
-        <title>{`${intl.formatMessage(messages.endOfCourseTitle)} | ${title} | ${getConfig().SITE_NAME}`}</title>
+        <title>{`${title} | ${getConfig().SITE_NAME}`}</title>
       </Helmet>
       <div className="row w-100 mx-0 mb-4 px-5 py-4 border border-light justify-content-center">
         <div className="col-12 p-0 h2 text-center">
--- src/tab-page/LoadedTabPage.jsx
+++ src/tab-page/LoadedTabPage.jsx
@@ -51,7 +51,7 @@ const LoadedTabPage = ({
         org={org}
       />
       <Helmet>
-        <title>{`${activeTab ? `${activeTab.title} | ` : ''}${title} | ${getConfig().SITE_NAME}`}</title>
+        <title>{`${getConfig().SITE_NAME}`}</title>
       </Helmet>
       {originalUserIsStaff && (
         <InstructorToolbar
